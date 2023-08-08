d1d16c0a2cd578216a51fc61830b23c6a60305fb
Andrzej Borucki <borucki.andrzej@gmail.com>
2023-08-08 22:16:22 +0200

    init with sources and themes

---
+++ lib/ksyntaxhighlighting_logging.h
@@ -0,0 +1,10 @@
+#ifndef KSYNTAXHIGHLIGHTING_LOGGING_H
+#define KSYNTAXHIGHLIGHTING_LOGGING_H
+
+#include <QDebug>
+
+#define qCWarning(Log) qDebug()
+#define qCDebug(Log) qDebug()
+#define qCCritical(Log) qDebug()
+
+#endif //KSYNTAXHIGHLIGHTING_LOGGING_H
