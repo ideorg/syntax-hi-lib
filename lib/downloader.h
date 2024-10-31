35999d6380c006c765866d3ecec725f476440937
Andrzej Borucki <borucki.andrzej@gmail.com>
2024-10-31 21:02:28 +0100

    format file, removed empty lines

--- lib/downloader.h
+++ lib/downloader.h
@@ -10,45 +10,31 @@ namespace KSyntaxHighlighting {
 class DownloaderUrl : public QObject {
 Q_OBJECT
     QNetworkAccessManager manager;
-
     void finished(QNetworkReply *reply);
-
     bool m_busy = false;
 public:
     QByteArray result;
     QNetworkReply::NetworkError error;
-
     bool start(QString fileUrl);
-
     bool busy() { return m_busy; }
-
 signals:
-
     void done();
 };
 
 class DownloaderUrls : public QObject {
 Q_OBJECT
     QNetworkAccessManager manager;
-
     void finished(QNetworkReply *reply);
-
     const int maxSimultaneousDownloads = 6;
     int currentDownloads = 0;
     QQueue<QNetworkRequest> downloadQueue;
-
     void addPortion();
-
     QString targetDir;
 public:
     void setTargetDir(QString targetDir);
-
     bool start(QStringList fileUrls);
-
     bool busy() { return currentDownloads || !downloadQueue.empty(); }
-
 signals:
-
     void done();
 };
 
@@ -63,49 +49,32 @@ protected:
     DownloaderUrl singleLoader;
     DownloaderUrls multiLoader;
     QSet<QString> fileSet;
-
     void readDir();
-
     bool startLoadSingle();
-
 public:
     bool busy() { return singleLoader.busy() || multiLoader.busy(); }
-
     void setPath(QString singlePath, QString multiPath);
-
     bool mustDownload();
-
     bool start();
-
     void finished();
-
 signals:
-
     void done();
 };
 
 class SyntaxDownloader : public AbstractDownloader {
     void finishedUpdate();
-
     std::pair<InfoList, InfoList> divideToTwoSets(InfoList &infoList);
-
     InfoList filterNewVersions(InfoList &infoList);
-
     QStringList getUrls(InfoList &infoList);
-
 public:
     SyntaxDownloader();
 };
 
 class ThemesDownloader : public AbstractDownloader {
     void finishedQrc();
-
     QStringList readQrcData(QByteArray &xmlData);
-
     static QStringList addPath(QStringList list, QString path);
-
     static QStringList filter(QStringList fileList, QSet<QString> &set);
-
 public:
     ThemesDownloader();
 };
@@ -118,21 +87,13 @@ Q_OBJECT
     QString path;
 public:
     DoubleDownloader();
-
     void setPath(QString path);
-
     bool mustDownload();
-
     bool start();
-
     void finishedFirst();
-
     void finishedLast();
-
     bool busy();
-
 signals:
-
     void done();
 };
 }
